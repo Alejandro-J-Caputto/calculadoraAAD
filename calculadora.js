6bb070bc86ae7ccde1e8bae877e12fa53d159c4a
diegoara <diegoarano41@hotmail>
2020-01-24 11:42:54 +0100

    funciones +,-,*,/

---
+++ calculadora.js
@@ -0,0 +1,57 @@
+
+
+
+ function sumar(num1,num2){
+  
+    let resultado = num1 + num2;        //suma
+
+    return resultado;
+  }
+
+  let suma = sumar(8,9);
+
+  alert(suma);
+
+  // document.getElementById("Pantalla").value = suma;
+  //-----------------------------------------------------------------------------------
+
+  function restar(num1, nume2) {
+
+    let resultado = num1 - nume2;     //resta
+
+    return resultado;
+      
+  }
+
+  let resta = restar (6,5);
+
+  alert(resta);
+
+//-----------------------------------------------------------------------------------------------
+
+function multiplicar(num1, num2) {
+
+    let resultado = num1 * num2;                  //multiplica
+
+    return resultado;
+}
+   let multiplica = multiplicar (7,8)
+
+   alert(multiplica)
+
+//------------------------------------------------------------------------------------------------
+
+
+function dividir(num1 , num2) {
+
+    let resultado = num1 / num2;              //dividir
+
+    return resultado;
+
+}
+ let divide = dividir (12, 3)
+
+ alert(divide)
+
+
+
